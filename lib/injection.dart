cb46994603eabb340f14383b0f681729f56ce4a1
Mathieu Meyer <meyer.mathieu-dev@mailo.com>
2025-10-01 12:18:12 +0200

    Change state items from list of ImageEntity to list of ImageUiModel ; implement FavoritesRepository in SearchScreenVM

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,23 +1,36 @@
+import 'package:carbon_it_images_search/data/repositories/hive_favorites_repository.dart';
 import 'package:carbon_it_images_search/data/repositories/pexels_images_search_repository.dart';
+import 'package:carbon_it_images_search/domain/repositories/favorites_repository.dart';
+import 'package:carbon_it_images_search/presentation/view_models/favorites_screen_view_model.dart';
 import 'package:carbon_it_images_search/presentation/view_models/search_screen_view_model.dart';
 import 'package:carbon_it_images_search/secrets.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'domain/repositories/images_search_repository.dart';
 
 final getItInstance = GetIt.instance;
 
 Future<void> setupInjection() async {
+  // Hive
+  await Hive.initFlutter();
+  final favoritesBox = await Hive.openBox<Map>(HiveFavoritesRepository.favoritesBoxName);
+
   // Repositories
   getItInstance.registerLazySingleton<ImagesSearchRepository>(
     () => PexelsImagesSearchRepository(dio: getItInstance<Dio>()),
   );
+  getItInstance.registerLazySingleton<FavoritesRepository>(() => HiveFavoritesRepository(favoritesBox: favoritesBox));
 
   // ViewModels
   getItInstance.registerLazySingleton<SearchScreenViewModel>(
-    () => SearchScreenViewModel(imagesSearchRepository: getItInstance<ImagesSearchRepository>()),
+    () => SearchScreenViewModel(
+      imagesSearchRepository: getItInstance<ImagesSearchRepository>(),
+      favoritesRepository: getItInstance<FavoritesRepository>(),
+    ),
   );
+  getItInstance.registerLazySingleton<FavoritesScreenViewModel>(() => FavoritesScreenViewModel());
 
   // Misc
   final apiKey = Secrets.apiKey;
--- lib/presentation/view_models/search_screen_view_model.dart
+++ lib/presentation/view_models/search_screen_view_model.dart
@@ -1,20 +1,35 @@
 import 'package:carbon_it_images_search/data/entities/image_entity.dart';
+import 'package:carbon_it_images_search/domain/repositories/favorites_repository.dart';
 import 'package:carbon_it_images_search/domain/repositories/images_search_repository.dart';
+import 'package:carbon_it_images_search/presentation/mappers/image_ui_model_mapper.dart';
+import 'package:carbon_it_images_search/presentation/models/image_ui_model.dart';
 import 'package:carbon_it_images_search/presentation/states/search_states.dart';
 import 'package:flutter/foundation.dart';
 
 class SearchScreenViewModel extends ChangeNotifier {
   final ImagesSearchRepository imagesSearchRepository;
+  final FavoritesRepository favoritesRepository;
   SearchState state = const SearchState.idle();
 
-  SearchScreenViewModel({required this.imagesSearchRepository});
+  SearchScreenViewModel({required this.imagesSearchRepository, required this.favoritesRepository});
 
   Future<void> submitSearch({required String searchValue}) async {
     state = state.copyWith(isLoading: true, imagesItems: [], errorMessage: null, lastQuery: searchValue);
     notifyListeners();
     try {
-      final List<ImageEntity> results = await imagesSearchRepository.searchImages(search: searchValue);
-      state = state.copyWith(isLoading: false, imagesItems: results, errorMessage: null, lastQuery: searchValue);
+      final List<ImageEntity> repositoryResults = await imagesSearchRepository.searchImages(search: searchValue);
+      final List<ImageUiModel> imagesToDisplay =
+          repositoryResults
+              .map(
+                (ImageEntity imageEntity) => ImageUiModelMapper.mapImageEntityToImageUiModel(imageEntity: imageEntity),
+              )
+              .toList();
+      state = state.copyWith(
+        isLoading: false,
+        imagesItems: imagesToDisplay,
+        errorMessage: null,
+        lastQuery: searchValue,
+      );
       notifyListeners();
     } catch (error) {
       state = state.copyWith(isLoading: false, imagesItems: [], errorMessage: error.toString(), lastQuery: searchValue);
@@ -22,7 +37,7 @@ class SearchScreenViewModel extends ChangeNotifier {
     }
   }
 
-  Future<void> addItemToFavorites({required ImageEntity imageEntity}) async {
+  Future<void> toggleItemFavorite({required ImageEntity imageEntity}) async {
     // TODO
   }
 }

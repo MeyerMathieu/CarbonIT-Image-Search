fc1b7a8cb04a15d32387c5fa44375f200c33ae54
Mathieu Meyer <meyer.mathieu-dev@mailo.com>
2025-10-02 13:14:53 +0200

    Convert SearchState to sealed class

--- lib/presentation/states/search_states.dart
+++ lib/presentation/states/search_states.dart
@@ -1,25 +1,25 @@
 import 'package:carbon_it_images_search/presentation/models/image_ui_model.dart';
 
-// TODO : Convert to sealed class
-class SearchState {
-  final bool isLoading;
+sealed class SearchState {
+  const SearchState();
+}
+
+final class SearchStateLoading extends SearchState {}
+
+final class SearchStateSuccess extends SearchState {
   final List<ImageUiModel> imagesItems;
-  final String? errorMessage;
+
+  const SearchStateSuccess(this.imagesItems);
+}
+
+final class SearchStateEmpty extends SearchState {
   final String? lastQuery;
 
-  const SearchState._({this.isLoading = false, this.imagesItems = const [], this.errorMessage, this.lastQuery});
+  const SearchStateEmpty({this.lastQuery});
+}
 
-  const SearchState.idle() : this._(lastQuery: null);
-  const SearchState.loading(String searchQuery) : this._(isLoading: true, lastQuery: searchQuery);
-  const SearchState.success(List<ImageUiModel> imagesItems) : this._(imagesItems: imagesItems);
-  const SearchState.errorMessage(String errorMessage) : this._(errorMessage: errorMessage);
+final class SearchStateError extends SearchState {
+  final String errorMessage;
 
-  SearchState copyWith({bool? isLoading, List<ImageUiModel>? imagesItems, String? errorMessage, String? lastQuery}) {
-    return SearchState._(
-      isLoading: isLoading ?? this.isLoading,
-      imagesItems: imagesItems ?? this.imagesItems,
-      errorMessage: errorMessage,
-      lastQuery: lastQuery ?? this.lastQuery,
-    );
-  }
+  const SearchStateError(this.errorMessage);
 }
